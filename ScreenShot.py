d136a511827ef28474e20195385599ca0d0a120e
HAYASAKA.Ryosuke <hayasaka.ryosuke@gmail.com>
2013-03-11 02:58:16 +0900

    firstcommit

---
+++ ScreenShot.py
@@ -0,0 +1,5 @@
+import time
+import commands
+while(True):
+	commands.getoutput("screencapture -C -x ./img1.png")
+	time.sleep(0.3)
---
+++ ScreenShot.sh
@@ -0,0 +1,5 @@
+while :
+do
+	sleep 0.3;
+	screencapture -C -x ./img1.png
+done
